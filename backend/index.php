21d0ba83aaf600e35a5ca7c9a4e8b428f9536fcc
Sarawut  Inree <the.last-stronger@live.com>
2017-10-05 22:10:32 +0700

    เพิ่มเติม
    แก้ไข unicode เป็น utf-8

--- backend/index.php
+++ backend/index.php
@@ -8,3 +8,15 @@ if(!empty($_GET['logged_out'])){
 }
 echo "<script>window.location='subject.php';</script>";
 ?>
+<!DOCTYPE html>
+<html lang="TH">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
