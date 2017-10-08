9235f0421d506ceaa612dba192b0008e6601e3d8
Sarawut  Inree <the.last-stronger@live.com>
2017-10-08 12:57:31 +0700

    เพิ่มการแก้ไขชั้นเรียน
    แก้ไขชั้นเรียน

--- backend/class.php
+++ backend/class.php
@@ -13,6 +13,25 @@ if(isset($_POST['add_cls'])){
             mysqli_query($conn,$str);
 
 }
+
+if(isset($_POST['confirmEdit'])){
+    
+    //โค้ดเพิมข้อมูลชั้นเรียนตรงนี้นะ
+    $class = $_POST['class'];
+    $name = $_POST['name'];
+    $lvl =  $_POST['lvl'];
+    $room = $_POST['room'];
+    $teacher = $_POST['teacher'];
+
+    $str = "UPDATE class SET
+            class_name = '$name',
+            class_lvl = '$lvl',
+            class_room = '$room',
+            teacher_id = '$teacher'
+            WHERE class_id = '$class'";
+            mysqli_query($conn,$str);
+
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -92,7 +111,7 @@ if(isset($_POST['add_cls'])){
                 </td>
                 <td>
                     <a href="student.php?class=<?php echo $cls['class_id'];?>" class="btn btn-warning btn-sm">รายชื่อนักเรียน</a>
-                    <a href="Java:void" class="btn btn-success btn-sm" data-toggle="modal" data-target="#editClass<?php echo $cls['class_id'];?>">แก้ไข</a>
+                    <a href="#" class="btn btn-success btn-sm" data-toggle="modal" data-target="#editClass<?php echo $cls['class_id'];?>">แก้ไข</a>
                     <!--Modalสำหรับแก้ไขชั้นเรียน-->
                 <div class="modal fade" id="editClass<?php echo $cls['class_id'];?>" tabindex="-1" role="dialog" aria-labelledby="editClass" aria-hidden="true">
                 <div class="modal-dialog" role="document">
@@ -106,14 +125,15 @@ if(isset($_POST['add_cls'])){
                     <div class="modal-body">
                         <form action="" method="post" >
                                 <div class="form-group">
+                                    <input type="hidden" name="class" value="<?php echo $cls['class_id'];?>">
                                     <label for="name" class="col-md-4">ระดับชั้น</label>
                                     <select name="name" id="name">
-                                        <option value="ปวช.">ปวช</option>
-                                        <option value="ปวส.">ปวส</option>
+                                        <option value="ปวช." <?php if($cls['class_name'] == 'ปวช.'){ echo "selected";}?>>ปวช</option>
+                                        <option value="ปวส." <?php if($cls['class_name'] == 'ปวส.'){ echo "selected";}?>>ปวส</option>
                                     </select>
                                     <label for="year" >ชั้นปี</label><label for="room" >ห้อง</label>
-                                    <input type="number" name="lvl" id="year" min="1" max="999" value="1">
-                                    <input type="number" name="room" id="room" min="1" max="999" value="1">
+                                    <input type="number" name="lvl" id="year" min="1" max="999"  value="<?php echo $cls['class_lvl'];?>" >
+                                    <input type="number" name="room" id="room" min="1" max="999" value="<?php echo $cls['class_room'];?>">
                                     <label for="class" class="col-md-4">ครูประจำชั้น</label>
                                     <select name="teacher">
                                             <?php 
@@ -121,7 +141,7 @@ if(isset($_POST['add_cls'])){
                                             $rs3 = mysqli_query($conn,$str3)or die(mysqli_error($conn));
                                             while($tea3 = mysqli_fetch_array($rs3)){
                                             ?>
-                                                <option value="<?php echo $tea3['teacher_id'];?>">อ.<?php echo $tea3['teacher_fname'];?> <?php echo $tea3['teacher_lname'];?></option>
+                                                <option value="<?php echo $tea3['teacher_id'];?>" <?php if($tea3['teacher_id'] == $cls['teacher_id']){ echo "selected";}?> >อ.<?php echo $tea3['teacher_fname'];?> <?php echo $tea3['teacher_lname'];?></option>
                                             <?php } ?>
                                             </select>
                                 </div>
