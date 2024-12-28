2c11872ee7fbe9712abe37435378a6fa08dcd287
Sarawut  Inree <sarawut.inree@gmail.com>
2024-12-28 09:06:57 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost";
-$user = "appdayin_reg";
-$pass = "lik;6tvboiupn";
-$database = "appdayin_reg";
+$user = "root";
+$pass = "";
+$database = "schoolreg";
 
 $conn = mysqli_connect($host,$user,$pass,$database);
 mysqli_query($conn,"set names utf8")
